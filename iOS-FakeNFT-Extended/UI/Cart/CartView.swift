b0dc42aa9374a536eb93d6efa340e09cf928b3f4
David Gordienko <grash002@yandex.ru>
2025-08-26 17:36:43 +0300

    feat: Add tabbar items

---
+++ iOS-FakeNFT-Extended/UI/Cart/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Иван Иван on 25.08.2025.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}
---
+++ iOS-FakeNFT-Extended/UI/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Иван Иван on 25.08.2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
